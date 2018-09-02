7bbabad93674fec008f0b0acf1d11538fcb934b8
srineshnisala <srineshnisala@gmail.com>
2018-09-02 21:16:13 +0530

    add executeScript wrapper

--- src/scripts/background/chrome-tab.ts
+++ src/scripts/background/chrome-tab.ts
@@ -1,5 +1,6 @@
 import CreateProperties = chrome.tabs.CreateProperties;
 import Tab = chrome.tabs.Tab;
+import InjectDetails = chrome.tabs.InjectDetails;
 
 /**
  * ChromeTab - wraps required chrome.tabs methods with Promise
@@ -20,6 +21,26 @@ export default class ChromeTab {
                 else
                     resolve(tab);
             })
+        }));
+    }
+
+    /**
+     * inject script to given page
+     *
+     * @param id
+     *      tab id script should be injected to
+     *
+     * @param injectDetails
+     *      details about the script
+     */
+    static executeScript(id: number, injectDetails: InjectDetails): Promise<Array<any>> {
+        return new Promise<Array<any>>(((resolve, reject) => {
+            chrome.tabs.executeScript(id, injectDetails, (result) => {
+                if (chrome.runtime.lastError)
+                    reject(chrome.runtime.lastError);
+                else
+                    resolve(result);
+            });
         }));
     }
 }
