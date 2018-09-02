0552de70be8811574bcce7583f4f8f2b7898b5c5
srineshnisala <srineshnisala@gmail.com>
2018-09-02 21:20:06 +0530

    make data parameter optional for CommandRequestModel constructor

--- src/scripts/command/command-request-model.ts
+++ src/scripts/command/command-request-model.ts
@@ -4,8 +4,9 @@ export default class CommandRequestModel {
     command: CommandType;
     data?:any;
 
-    constructor(command: CommandType, data: any) {
+    constructor(command: CommandType, data?: any) {
         this.command = command;
-        this.data = data;
+        if(data)
+            this.data = data;
     }
 }
