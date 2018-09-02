f050a63c0004139ec5965b41095ea6972441675c
srineshnisala <srineshnisala@gmail.com>
2018-09-02 13:41:21 +0530

    add process variable
    process is declared to avoid compile time errors when code access process.env.NODE_ENV

---
+++ src/scripts/common/process.ts
@@ -0,0 +1,10 @@
+/**
+ * this is just a variable to avoid compilation error when using process.env.NODE_ENV
+ * in code
+ * webpack will replace process.env.NODE_ENV with actual value at compile time
+ */
+declare var process : {
+    env: {
+        NODE_ENV: string
+    }
+};
