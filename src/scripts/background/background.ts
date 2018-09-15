bf01c93117bacd192aaa7628f2f1d5acd1a88c89
srineshnisala <srineshnisala@gmail.com>
2018-09-15 21:32:39 +0530

    change of comments

--- src/scripts/background/background.ts
+++ src/scripts/background/background.ts
@@ -4,12 +4,13 @@
 import ChromeWindow from "./chrome-window";
 import CommandManager from "./command-manager";
 
+/*
+handling commands externalized to a separate async method
+making addListener callback an async will hold the return true statement until completion of async methods
+as a result sender callback will be called due to `undefined` return
+ */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    /*
-    handling commands externalized to a separate async method
-    making addListener callback an async will hold the return true statement until completion of async methods
-    as a result sender callback will be called due to `undefined` return
-     */
+
     CommandManager.handleCommands(request, sender, sendResponse);
 
     // return true will make sure senders callback will wait until sendResponse is called
