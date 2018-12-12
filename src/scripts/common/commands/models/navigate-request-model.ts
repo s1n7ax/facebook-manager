0b86e482600becb43d565143f272ebabfba352e7
srineshnisala <srineshnisala@gmail.com>
2018-12-13 00:29:55 +0530

    extends from ICommandRequestModel

--- src/scripts/common/commands/models/navigate-request-model.ts
+++ src/scripts/common/commands/models/navigate-request-model.ts
@@ -1,7 +1,12 @@
-export default class NavigateRequestModel {
+import ICommandRequestModel from "./i-command-request-model";
+import CommandType from "../command-type";
+
+export default class NavigateRequestModel implements ICommandRequestModel {
+    readonly command = CommandType.NAVIGATE;
     url: string;
 
     constructor(url: string) {
         this.url = url;
     }
+
 }
